2d397951bb73bad5acf048edd0e82625500f4c12
Yash Shah <yashshah569@gmail.com>
2023-01-13 09:58:13 +0530

    Time: 3 ms (95.97%), Space: 7.5 MB (57.86%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -4,25 +4,13 @@ public:
         int len = s.size();
         string str;
         for(int i=0;i<len;i++){
-            if(s[i]>=65 && s[i]<=90){
-                str+=(s[i]+32);
-            }
-            else if(s[i]>=97 && s[i]<=122){
-                str+=s[i];
-            }
-            else if(s[i]>=48 && s[i]<=57){
-                str+=s[i];
-            }
-            else{
-                continue;
+            if(isalnum(s[i])){
+                str.push_back(tolower(s[i]));
             }
         }
         int str_len=str.size();
         for(int i=0;i<str_len/2;i++){
-            if(str[i]==str[str_len-1-i]){
-                continue;
-            }
-            else{
+            if(str[i]!=str[str_len-1-i]){
                 return false;
             }
         }
