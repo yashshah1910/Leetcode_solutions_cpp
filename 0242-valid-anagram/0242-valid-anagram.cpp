e74b18a20d24840bdda6d47a3334b55ba6b9ec98
Yash Shah <yashshah569@gmail.com>
2023-01-13 14:07:38 +0530

    Time: 13 ms (68.71%), Space: 7.4 MB (16.19%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -3,13 +3,13 @@ public:
     bool isAnagram(string s, string t) {
         if(s.size()!=t.size())
             return false;
-        int n=s.size(), count[26]={0};
-        for(int i=0;i<n;i++){
-            count[s[i]-'a']++;
+        unordered_map <char, int> count;
+        for(int i=0;i<s.size();i++){
+            count[s[i]]++;
+            count[t[i]]--;
         }
-        for(int i=0;i<n;i++){
-            count[t[i]-'a']--;
-            if(count[t[i]-'a']<0)
+        for(auto i: count){
+            if(i.second)
                 return false;
         }
         return true;
